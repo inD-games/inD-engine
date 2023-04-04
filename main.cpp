87c855e1fe0088511f93345ef7d18c9eaf70e513
Acar Okan AKALIN <acar.okan.akalin@ind.games>
2023-04-04 22:50:51 +0300

    openAL

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@
 
 #include "VulkanRenderer.h"
 
+#include "SoundDevice.h"
+#include "SoundBuffer.h"
+#include "SoundSource.h"
+
 GLFWwindow * window;
 VulkanRenderer vulkanRenderer;
 
@@ -32,6 +36,13 @@ int main()
     {
         return EXIT_FAILURE;
     }
+
+    SoundDevice * mysounddevice = SoundDevice::get();
+    uint32_t /*ALuint*/ sound1 = SoundBuffer::get()->addSoundEffect("../res/spellsounds/spell.ogg");
+    uint32_t /*ALuint*/ sound2 = SoundBuffer::get()->addSoundEffect("../res/spellsounds/magicfail.ogg");
+    SoundSource mySpeaker;
+    mySpeaker.Play(sound1);
+    mySpeaker.Play(sound2);
 
     // Loop until closed
     while (!glfwWindowShouldClose(window))
