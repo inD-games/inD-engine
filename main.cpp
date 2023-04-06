a5373fb1ea38f55f84ca28bdfe3ae31a8759e7e6
Acar Okan AKALIN <acar.okan.akalin@ind.games>
2023-04-06 23:12:24 +0300

    various dependencies

--- main.cpp
+++ main.cpp
@@ -1,60 +1,6 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#include <stdexcept>
-#include <vector>
 #include <iostream>
 
-#include "VulkanRenderer.h"
-
-#include "SoundDevice.h"
-#include "SoundBuffer.h"
-#include "SoundSource.h"
-
-GLFWwindow * window;
-VulkanRenderer vulkanRenderer;
-
-void initWindow(std::string wName = "Test Window", const int width = 800, const int height = 600)
-{
-    // Initialise GLFW
-    glfwInit();
-
-    // Set GLFW to NOT work with OpenGL
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
-    window = glfwCreateWindow(width, height, wName.c_str(), nullptr, nullptr);
-}
-
-int main()
-{
-    // Create Window
-    initWindow("Test Window", 800, 600);
-
-    // Create Vulkan Renderer instance
-    if (vulkanRenderer.init(window) == EXIT_FAILURE)
-    {
-        return EXIT_FAILURE;
-    }
-
-    SoundDevice * mysounddevice = SoundDevice::get();
-    uint32_t /*ALuint*/ sound1 = SoundBuffer::get()->addSoundEffect("../res/spellsounds/spell.ogg");
-    uint32_t /*ALuint*/ sound2 = SoundBuffer::get()->addSoundEffect("../res/spellsounds/magicfail.ogg");
-    SoundSource mySpeaker;
-    mySpeaker.Play(sound1);
-    mySpeaker.Play(sound2);
-
-    // Loop until closed
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwPollEvents();
-    }
-
-    vulkanRenderer.cleanup();
-
-    // Destroy GLFW window and stop GLFW
-    glfwDestroyWindow(window);
-    glfwTerminate();
-
+int main() {
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
